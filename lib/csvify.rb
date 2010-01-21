7cd13e27bce2216324682ef7307c39ee0ba15d72
unknown <Eli Mayost@.(none)>
2010-01-21 19:50:08 +0000

    master

--- lib/csvify.rb
+++ lib/csvify.rb
@@ -1,23 +1,15 @@
-module Csvify  
-  def self.included(base)  
-    base.extend ClassMethods  
-  end  
-  
-  module ClassMethods  
-    def to_csv
-      columns = self.first.class.column_names
-      data = ""
-      self.each do |o|
-        columns.each_with_index do |c, i|
-          data << "'#{o.send(c.to_s).to_s.strip}', "
-        end
-        data << "\n"
+class Array
+
+  def to_csv
+    columns = self.first.class.column_names
+    data = ""
+    self.each do |o|
+      columns.each_with_index do |c, i|
+        data << "'#{o.send(c.to_s).to_s.strip}', "
       end
-      return data
+      data << "\n"
     end
+    return data
   end
-end  
-  
-class Array  
-  include Csvify  
-end  
+
+end
