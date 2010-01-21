e51df871a3626a6840d1118fa16e8650e45ec1d8
unknown <Eli Mayost@.(none)>
2010-01-21 20:12:05 +0000

    master

--- lib/csvify.rb
+++ lib/csvify.rb
@@ -6,9 +6,9 @@ class Array
     self.each do |o|
       columns.each_with_index do |c, i|
         if i < columns.length-1
-          data << "'#{o.send(c.to_s).to_s.strip}'"
-        else
           data << "'#{o.send(c.to_s).to_s.strip}', "
+        else
+          data << "'#{o.send(c.to_s).to_s.strip}'"
         end
       end
       data << "\n"
